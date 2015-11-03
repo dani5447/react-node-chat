c83676df5c545136093d7aae001ae00d634949fd
dani5447 <drn5447@rit.edu>
2015-11-02 22:32:32 -0500

    Init Commit with Logic/Components

---
+++ app/js/chat.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Chat from './components/chat';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar page="Chat"/>
+        <div className="container">
+          <Chat />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render((
+  <App />
+), document.getElementById('main'));
